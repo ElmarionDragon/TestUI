a8b3486958bb711e2fa21cc7c7f6692970ac2fa1
Leonid <110391660+ElmarionDragon@users.noreply.github.com>
2022-08-02 13:52:36 +1000

    slider

--- Assets/UI/Slider/Slider.cs
+++ Assets/UI/Slider/Slider.cs
@@ -10,9 +10,56 @@ namespace TestUI
         [UnityEngine.Scripting.Preserve]
         public new class UxmlFactory : UxmlFactory<Slider> { }
 
+        private const string styleResource = "SliderUSS";
+
+        private const string ussContainer = "slider_container";
+        private const string ussWindow = "slider_window";
+        private const string ussTitle = "slider-title";
+        private const string ussSlider = "slider";
+        private const string ussTextField = "slider-text";
+
+        UnityEngine.UIElements.Slider slider;
+        private TextField textField;
+
         public Slider()
         {
+            styleSheets.Add(Resources.Load<StyleSheet>(styleResource));
+            AddToClassList(ussContainer);
 
+            VisualElement window = new VisualElement();
+            window.AddToClassList(ussWindow);
+            hierarchy.Add(window);
+
+            Label title = new Label();
+            title.AddToClassList(ussTitle);
+            window.Add(title);
+            title.text = "Размер";
+
+            slider = new UnityEngine.UIElements.Slider(1,10);
+            slider.AddToClassList(ussSlider);
+            window.Add(slider);
+            slider.RegisterValueChangedCallback(v =>
+            {
+                string newValue = v.newValue.ToString();
+                textField.SetValueWithoutNotify(newValue);
+                onValueChanged(v.newValue);
+            });
+
+            textField = new TextField();
+            textField.AddToClassList(ussTextField);
+            window.Add(textField);
+            textField.SetValueWithoutNotify("1");
+            textField.RegisterValueChangedCallback(v =>
+            {
+                float newValue = float.Parse(v.newValue);
+                slider.SetValueWithoutNotify(newValue);
+                onValueChanged(newValue);
+            });
+        }
+
+        public void onValueChanged(float v)
+        {
+            Debug.Log("onValueChanged: " + v);
         }
     }
 }
